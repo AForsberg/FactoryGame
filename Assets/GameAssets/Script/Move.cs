f772ad82ae60e659d77a3536b31d67cfd68a934b
sebbeOMG <sebb.sebbe@gmail.com>
2014-09-22 20:15:42 +0200

    BIG UPDATE! Crushers + scene!

---
+++ Assets/GameAssets/Script/Move.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public Transform pos_top;
+	public Transform pos_bot;
+
+	private bool goingUp = false;
+	public float speedUp = 2f;
+	public float speedDown = 4f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (goingUp) {
+			transform.position = Vector3.MoveTowards(transform.position, pos_top.position, Time.deltaTime*speedUp);
+
+			if (transform.position == pos_top.position)
+				goingUp = false;
+		}
+
+		if (!goingUp) {
+			transform.position = Vector3.MoveTowards (transform.position, pos_bot.position, Time.deltaTime * speedDown);
+
+			if (transform.position == pos_bot.position) {
+				goingUp = true;
+
+				// play some ground hit sound effect
+				audio.Play();
+			}
+		}
+	}
+}
