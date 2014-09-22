1330f2e05e63fffb8d98fca6f686f68559e6c942
Anton Forsberg <anton.forsberg1@gmail.com>
2014-09-22 15:25:31 +0200

    Rifle added, changed gun mechanics, old gun gone

--- Assets/GameAssets/Script/Gun.cs
+++ Assets/GameAssets/Script/Gun.cs
@@ -5,13 +5,16 @@ public class Gun : MonoBehaviour
 {
 	public Rigidbody2D projectile;	// Prefab of the projectile.
 	public float speed = 20f;		// The speed the projectile will fire at.
+	public bool autoWeapon;			// Is the weapon auto or singleshot?
+	public float shotrate;			// Time between shots in seconds
 	Transform projectile_spawn;
 	Transform muzzleFlash;
 	
 	private Player player;			// Reference to the PlayerControl script.
 	//private Animator anim;		// Reference to the Animator component.
 	
-	
+	float cooldown;
+
 	void Awake()
 	{
 		// Setting up the references.
@@ -22,34 +25,35 @@ public class Gun : MonoBehaviour
 	}
 	void Start(){
 		projectile_spawn = GameObject.FindGameObjectWithTag("projectile spawn").transform;
-		muzzleFlash = GameObject.FindGameObjectWithTag("MuzzleFlash").transform;//<MuzzleFlash>().Flash();
+		muzzleFlash = GameObject.FindGameObjectWithTag("MuzzleFlash").transform;
+		if(!autoWeapon){
+			shotrate = 0.5f;
+		}
+
+		cooldown = 0f;
 	}
 	
 	void Update ()
 	{
+		if(cooldown > 0){
+			cooldown -= Time.deltaTime;
+		}
 		// If the fire button is pressed...
-	if(Input.GetKeyDown(KeyCode.F))
+		if(Input.GetKey(KeyCode.F) && canShoot())
 		{
-			/*
-			// create a projectile
-			Rigidbody2D bulletInstance = Instantiate(projectile_laser, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-			bulletInstance.velocity = new Vector2(speed, 0);
-			*/
 
-			// ... set the animator Shoot trigger parameter and play the audioclip.
-			//anim.SetTrigger("Shoot");
-			//audio.Play();
+			cooldown = shotrate;
 
 			// If the player is facing right...
 			if(player.facingRight)
 			{
-				// ... instantiate the rocket facing right and set it's velocity to the right. 
+				// ... instantiate the projectile facing right and set it's velocity to the right. 
 				Rigidbody2D projectileInstance = Instantiate(projectile, projectile_spawn.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
 				projectileInstance.velocity = new Vector2(speed, 0);
 			}
 			else
 			{
-				// Otherwise instantiate the rocket facing left and set it's velocity to the left.
+				// Otherwise instantiate the projectile facing left and set it's velocity to the left.
 				Rigidbody2D bulletInstance = Instantiate(projectile, projectile_spawn.position, Quaternion.Euler(new Vector3(0,0,180f))) as Rigidbody2D;
 				bulletInstance.velocity = new Vector2(-speed, 0);
 			}
@@ -60,5 +64,8 @@ public class Gun : MonoBehaviour
 			// play some "gun-fire" sound
 			audio.Play();
 		}
+	}
+	bool canShoot(){
+		return cooldown <= 0f;
 	}
 }
