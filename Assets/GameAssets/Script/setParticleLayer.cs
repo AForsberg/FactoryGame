063cd1ba9c6cd920b45be3eeb42db07c6c842542
sebbeOMG <sebb.sebbe@gmail.com>
2014-09-24 11:30:06 +0200

    Added "fire" to enemy sprite

---
+++ Assets/GameAssets/Script/setParticleLayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setParticleLayer : MonoBehaviour {
+
+	public string layer = "Character";
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.sortingLayerName = layer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
