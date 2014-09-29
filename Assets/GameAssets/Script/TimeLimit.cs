40adf5e60d3de9694c2c5154ff8e7bb3fcc968dc
Anton Forsberg <anton.forsberg1@gmail.com>
2014-09-29 11:32:16 +0200

    Added timelimit

---
+++ Assets/GameAssets/Script/TimeLimit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeLimit : MonoBehaviour {
+
+	public float timeLimit = 30f;		// Timelimit in seconds.
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timeLimit <= 0){
+			Destroy(player);
+			return;
+		}		
+		timeLimit -= Time.deltaTime;
+	}
+
+	void OnGUI(){
+		GUI.Box(new Rect(210, 10, 250, 22), "Time left: " + (int)timeLimit);
+	}
+}
