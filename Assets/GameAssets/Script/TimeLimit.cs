c922b28b4e5fa3410cbf87a82e5935dfaef9c6fb
sebbeOMG <sebb.sebbe@gmail.com>
2014-09-30 23:03:01 +0200

    Added Huge Explosion when timer ends
    + beep effect for timer

--- Assets/GameAssets/Script/TimeLimit.cs
+++ Assets/GameAssets/Script/TimeLimit.cs
@@ -4,17 +4,42 @@ using System.Collections;
 public class TimeLimit : MonoBehaviour {
 
 	public float timeLimit = 30f;		// Timelimit in seconds.
+	public GameObject explosion;
+	
+	public int soundCountdownStart = 5;
+	private int soundWarning;
+
 	private GameObject player;
+
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
+		soundWarning = soundCountdownStart;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(timeLimit <= 0){
+
+		// play some "alarming/stressfull" sound when getting close to 0.
+		// +1 make first beep appear at sound soundCountdownStart.
+		// timeLimit > 0 removes the last beep. (Don't want beep and explosion sound at the same time)
+		if (timeLimit <= soundWarning + 1 && timeLimit > 0) { 
+			// set next beep for next sec.
+			soundWarning --;
+			// play some sound (beep)
+			audio.Play();
+		}
+
+		if (timeLimit <= 0){
+
+			// activate explosion
+			explosion.SetActive(true);
+
+			// kill the player
 			Destroy(player);
+
+			// Done...
 			return;
 		}		
 		timeLimit -= Time.deltaTime;
